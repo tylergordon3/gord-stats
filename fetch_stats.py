d0cdfdd4762628684df78ae6a4928b3eee24f07f
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2025-10-15 19:43:50 +0000

    adding more to get stats

--- fetch_stats.py
+++ fetch_stats.py
@@ -3,13 +3,18 @@
 '''
 import nflreadpy as nfl
 import pandas as pd
-
+import constants
 
 # Load player game-level stats for multiple seasons
-player_stats = nfl.load_player_stats([2025])
-
+player_stats = nfl.load_player_stats(2025, 'week')
 
 # nflreadpy uses Polars instead of pandas. Convert to pandas if needed:
-pbp_pandas = pd.DataFrame(player_stats)
+p_stats = player_stats.to_pandas()
+p_stats.columns = constants.player_stats_headers
+print(p_stats.describe())
+pos_groups_remove = ["LB", "DL", "OL", "DB", "None"]
+spec_teams_remove = ["P", "LS"]
+p_stats_filter = p_stats[~p_stats.position_group.isin(pos_groups_remove)]
+p_stats = p_stats_filter[~p_stats_filter.position.isin(spec_teams_remove)]
 
-print(pbp_pandas)
+print(p_stats.describe())
