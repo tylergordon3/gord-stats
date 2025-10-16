848918ac6f2d58470c5e135451c4b84fb4aa992c
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2025-10-16 21:00:26 +0000

    able to get a team's players from a specific week

--- fantasy_rosters.py
+++ fantasy_rosters.py
@@ -3,6 +3,9 @@
 """
 from sleeper_wrapper import League
 import pandas as pd
+import re
+import json
+import fantasy_stats
 
 
 def get(league) -> pd.DataFrame:
@@ -35,7 +38,31 @@ def get(league) -> pd.DataFrame:
     rosters = pd.merge(rosters, users, on='owner_id', how='inner')
     rosters = rosters.drop(
         columns=['fpts', 'fpts_decimal', 'fpts_against', 'fpts_against_decimal'])
+    
+    
     return rosters
+   
+    
+def getTeam(rosters, team_name, week):
+    myTeam = rosters[rosters['team_name'] == team_name]
+    starters = myTeam['players'].values[0]
+
+    with open('players.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        players = pd.DataFrame.from_dict(data, orient='index')
+
+    players_filter = players[players.player_id.isin(starters)]
+    # sleeper players for chosen team
+    team = players_filter[['search_full_name', 'team', 'fantasy_positions', 'fantasy_data_id']]
+    
+    wk = fantasy_stats.get(week)
+    wk['name_match'] = [re.sub(r'\s+', '', str(x)).lower() for x in wk['player_display_name']]
+    wk_me = wk[wk.name_match.isin(team['search_full_name'])]
+    
+    # fantasy stats for players on chosen team
+    print(wk_me)
+
+    return wk_me
 
 
 myLeague = League(league_id="1257466498994143232")
--- median.py
+++ median.py
@@ -1,9 +1,14 @@
 import nflreadpy as nfl
-import pandas as pd
+from sleeper_wrapper import League
 import constants as c
-import fantasy_stats
+import fantasy_rosters
 
 week = nfl.get_current_week()
-week = week -1
-stats = fantasy_stats.get(week)
-print(stats.describe().round(2))
+prev_week = week - 1
+#stats = fantasy_stats.get(prev_week)
+
+league = League(c.LEAGUEID)
+rosters = fantasy_rosters.get(league)
+
+team_last_week = fantasy_rosters.getTeam(rosters, "Big Gourds", week=prev_week)
+print(team_last_week)
