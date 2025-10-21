0a8152575362e7d004f466015264bce6def666a7
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2025-10-21 21:30:08 +0000

    testing roster mapping

--- fantasy_rosters.py
+++ fantasy_rosters.py
@@ -41,30 +41,3 @@ def get(league) -> pd.DataFrame:
     
     
     return rosters
-   
-    
-def getTeam(rosters, team_name, week):
-    myTeam = rosters[rosters['team_name'] == team_name]
-    starters = myTeam['players'].values[0]
-
-    with open('players.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-        players = pd.DataFrame.from_dict(data, orient='index')
-
-    players_filter = players[players.player_id.isin(starters)]
-    # sleeper players for chosen team
-    team = players_filter[['search_full_name', 'team', 'fantasy_positions', 'fantasy_data_id']]
-    
-    wk = fantasy_stats.get(week)
-    wk['name_match'] = [re.sub(r'\s+', '', str(x)).lower() for x in wk['player_display_name']]
-    wk_me = wk[wk.name_match.isin(team['search_full_name'])]
-    
-    # fantasy stats for players on chosen team
-    print("RESULTS FOR WEEK: ", week)
-    print(wk_me)
-
-    return wk_me
-
-
-myLeague = League(league_id="1257466498994143232")
-teams = get(league=myLeague)
--- median.py
+++ median.py
@@ -3,13 +3,19 @@ from sleeper_wrapper import League
 import constants as c
 import fantasy_rosters, fantasy_stats
 import player_db as db
+import fantasy_rosters as fr
 
-week = nfl.get_current_week()
-prev_week = week
 #stats = fantasy_stats.get(prev_week)
-
 league = League(c.LEAGUEID)
 rosters = fantasy_rosters.get(league)
 
+leagueData = fr.get(league)
+
+BigGourds = leagueData.loc[1]
+
 players = db.get(7)
-print(players)
+print(BigGourds.starters[0])
+
+# merged_players[~merged_players.position.isin(spec_teams_remove)]
+team = players[players.sleeper_id.isin(BigGourds.starters)]
+print(team)
