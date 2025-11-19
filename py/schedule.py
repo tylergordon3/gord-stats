10b4bfbc925aba11c16b22e25ee10148a7a8e8db
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2025-11-19 17:50:09 +0000

    set up background for finding record vs all schedules

--- py/schedule.py
+++ py/schedule.py
@@ -33,6 +33,7 @@ def saveSchedules():
     rosters = schedScores(rosters)
     rosters['myScores'] = rosters.apply(lambda x: np.repeat(x['roster_id'], 14), axis=1)
     rosters['myScores'] = rosters['myScores'].apply(lambda x: getScoreArr(x))
+    rosters['wins_vs'] = rosters.apply(lambda x: recordsVsAll(rosters, x), axis=1)
     util.save_df_to_json(rosters, f'data/rost{yr}_{wk}.json')
     
 def getScore(roster_id, week):
@@ -49,13 +50,46 @@ def schedScores(rosters):
     rosters['sched_score'] = rosters['sched'].apply(lambda x: getScoreArr(x))
     return rosters
 
+def recordsVsAll(all, team):
+    # For current team, calculate wins vs each schedule
+    curr_team = team['myScores']
+    wins_vs_each_arr = all['sched_score'].apply(lambda opp: recordVsHelper(curr_team, opp))
+    return list(wins_vs_each_arr)
+
+def recordVsHelper(team_scores, opp_scores):
+    # For current team calculate wins vs 1 opponents schedule
+    np_team = np.array(team_scores, dtype='float32')
+    np_opp= np.array(opp_scores, dtype='float32')
+    bool_arr = (np_team > np_opp)
+    wins = bool_arr.sum()
+    return wins
+
+
+def dictVsAllSched(rosters):
+    yr = util.getYrStr()
+    wk = util.get_week()
+    rosters = util.load_df_from_json(f'data/rost{yr}_{wk}.json')
+    all_results = {}
+    for index, row in rosters.iterrows():
+        # index, value in enumerate(my_array)
+        arr = {}
+        for index, val in enumerate(row['wins_vs']):
+            name = rosters[rosters['roster_id'] == index+1]['team_name']
+            arr[list(name)[0]] = val
+        all_results[row['roster_id']] = arr
+    return all_results
+
 def Sos():
+    return
+    
+def main():
+    update = False
     yr = util.getYrStr()
     wk = util.get_week()
     checkPath = f'data/rost{yr}_{wk}.json'
-    if not os.path.exists(checkPath):
+    if not os.path.exists(checkPath) or (update == True):
        saveSchedules()
     rosters = util.load_df_from_json(checkPath)
+    all = dictVsAllSched(rosters)
 
-league = League(cons.LEAGUEID)
-Sos()
+main()
