a7fa6640615bc9b29b58604427e1cec4cad61bdc
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2025-12-03 15:46:46 +0000

    fix arrows for init predict

--- py/change.py
+++ py/change.py
@@ -10,6 +10,7 @@ from io import StringIO
 def change(date):
     week_ago = date - timedelta(days=7)
     week_ago_path = utils.get_recent_html(week_ago)
+    today_path = utils.get_recent_html(date)
     with open(week_ago_path) as fp:
         soup = BeautifulSoup(fp, 'html.parser')
     table = soup.find("table")
@@ -17,5 +18,8 @@ def change(date):
     def getOvr(x):
         splt = x.split()
         return splt[2][2:-1]
-    df['vs Last Wk'] = df['Overall'].apply(lambda x: getOvr(x))
+    if today_path == week_ago_path:
+        df['vs Last Wk'] = 'NR'
+    else:
+        df['vs Last Wk'] = df['Overall'].apply(lambda x: getOvr(x))
     return df[['Team', 'vs Last Wk']].copy()
