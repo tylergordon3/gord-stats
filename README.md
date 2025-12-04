d433cb741a92ecc249faeaa091193f009fa28151
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2025-12-04 22:10:33 +0000

    scaped all mens logos

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Repo for Bracket Gordology. 
 
 Website with Men's College Basketball predicitons for which teams will qualify for March Madness using machine learning with Kenpom and Torvik data from 2013-present.
+
+
--- py/scraper.py
+++ py/scraper.py
@@ -7,15 +7,18 @@ import re
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import utils
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from datetime import datetime
-
+import os
 import random
 
 
 from playwright.sync_api import sync_playwright
+
+import os
 
 TORVIK_PRE = "https://barttorvik.com/trankpre.php"
 KENPOM = "https://kenpom.com/"
@@ -28,27 +31,38 @@ def getHTML(link, retries=5, base_delay=1.0):
         if response.status_code == 429 : 
             if attempt < retries - 1:
                 delay = base_delay * (2 ** attempt) + random.uniform(0, 0.2)
+                print(f'Sleeping for: {delay} seconds.')
                 time.sleep(delay)
                 continue
         if response.status_code == 200:
-            content = response.text()
+            content = response.text
             return BeautifulSoup(content, "html.parser")
     return None  # if all retries fail
 
 def pull_sportsDB():
-    #https://www.thesportsdb.com/api/v1/json/123/search_all_teams.php?l=English_Premier_League
-    #https://www.thesportsdb.com/api/v1/json/123/search_all_teams.php?s=Soccer&c=Spain
-    # {strLeague} {strCountry} {strSport}
     strLeague = 'NCAA_Division_I_Basketball_Mens'
-    league = f'https://www.thesportsdb.com/api/v1/json/123/search_all_teams.php?l={strLeague}'
     teams = pd.read_json(utils.get_path('data/team_list.json'))
     link = 'https://www.thesportsdb.com/api/v1/json/123/searchteams.php?t='
-    for team in list(teams[0]):
-        web = link + team
-        res = getHTML(web)
-        print(res)
 
-pull_sportsDB()
+    for team in list(teams[0]):
+        check = utils.get_path(f'docs/assets/images/{team}.png')
+        if not os.path.exists(check):
+            print(f'Querying for: {team}')
+            web = link + team
+            try:
+                pattern = r'\"strBadge\":\"(https:\\\/\\\/r2\.thesportsdb\.com\\\/images\\\/media\\\/team\\\/badge\\\/[A-Za-z0-9]+\.png)\"'
+                badge =  re.findall(pattern,  requests.get(web).text)
+                cl = badge[0].replace('\\/', '/')
+                img_data = requests.get(cl).content
+                with open(utils.get_path(f'docs/assets/images/{team}.png'), 'wb') as handler:
+                    handler.write(img_data)
+                print(f'Done for: {team}')
+            except:
+                print(f'Error for: {team}')
+            print(f'Sleeping for 8 seconds')
+            time.sleep(8)
+        
+
 
 def kenpom_historic():
     # https://kenpom.com/index.php?y=2025
