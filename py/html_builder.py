a1b2c8464abe4942301059fe889f0c6ac5aede34
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2025-12-15 17:28:28 +0000

    updates to index

--- py/html_builder.py
+++ py/html_builder.py
@@ -76,7 +76,7 @@ def generate_index():
         standings
         .style
         .hide(axis="index") 
-        .format( lambda x: f"{x:.3f}" if isinstance(x, (int, float)) else x) 
+        .format( lambda x: f"{x:.3f}" if isinstance(x, float) else x) 
         .background_gradient(cmap="RdYlGn_r", subset=["SOS"]) 
         .background_gradient(cmap="RdYlGn", subset=["SOV"])
         .background_gradient(cmap="RdYlGn_r", subset=["Scoring Luck"])
@@ -106,12 +106,6 @@ def generate_index():
     <div class="table-scroll">
     {styler.to_html(classes='sticky-table')}
     </div>
-    <br><h3>Site Update Log</h3>
-    <p>Tue 12/09/25 -  7 am - Site updated for week 14, regular season completed</p>
-    <p>Sun 12/07/25 -  9 pm - Updated stats for all week 14 games up to SNF</p>
-    <p>Tue 12/02/25 - 12 pm - Updated all pages post week 13, updated playoff scenarios on home.</p>
-    <p>Mon 12/01/25 -  7 am - Updated week 13 pages with all games prior to MNF & playoff scenarios.</p>
-    <p>Sun 11/30/25 -  9 pm - Updated Edited median to (hopefully) stop including injured players as "To Play"</p>
   </body>
 </html>
 '''
