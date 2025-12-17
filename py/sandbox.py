ee9e08083d2cf7650e72414b6b5857104baf82b0
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2025-12-17 19:58:09 +0000

    fixing images

--- py/sandbox.py
+++ py/sandbox.py
@@ -4,10 +4,41 @@ import os
 import pandas as pd
 import scraper
 
+def upd():
+    master = scraper.getMasterTeams()
+    for index, row in master.iterrows():
+        new = row['team'].lower()
+        new = new.replace('.', '')
+        new = new.replace(' ', '')
+        namearr = row['names']
+        if new in row.names:
+            pass
+        else:
+            namearr.append(new)
+            master.at[index, 'names'] = namearr
+    scraper.saveMasterTeams(master)
 
-orig = pd.read_json(utils.get_path('data/teams/master copy.json'))
-new = pd.read_json(utils.get_path('data/teams/master.json'))
+path = utils.get_path('docs/assets/images')
 
-diff = new['team'].compare(orig['team'])
+directory = os.listdir(path)
+master = scraper.getMasterTeams()
+master['image'] = sorted(directory)
+master['image'] = master['image'].apply(lambda x: x[:-4])
 
-print(diff.to_string())
+def check(row):
+    s_exploded = master["names"].explode()
+    boolean_mask_exploded = s_exploded == row.image
+    # To get the row IDs where the value is present:
+    matching_ids = s_exploded[boolean_mask_exploded].index.unique()
+    boolean_mask_original = boolean_mask_exploded.groupby(level=0).any()
+    df_result = master[boolean_mask_original]
+   
+    if df_result.empty:
+        return ''
+    else:
+        return matching_ids
+    
+master['team'] = master['team'].replace('.','')
+master['check'] = master.apply(lambda x: check(x), axis=1)
+print(master[master['check'] == ''])
+
