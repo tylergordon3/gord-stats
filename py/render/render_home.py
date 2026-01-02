d6bffa49d3acaf37efdeaa487f0078b51bc5f45e
tylergordon3 <tmgordon33@gmail.com>
2026-01-02 00:07:53 -0500

    1/2 upd

--- py/render/render_home.py
+++ py/render/render_home.py
@@ -41,8 +41,12 @@ def render_home(men_rankings, men_games_html, women_rankings, women_games_html):
         men_games = "No men's power 5 games today."
     if women_games == "":
         women_games = "No women's power 5 games today."
-
-    html = "<h3>Men's Power 5 Games Today</h3>" + men_games + "<h3>Women's P5 Games Today</h3>" + women_games
+    html = '''
+    <p>Using machine learning to predict the NCAA March Madness field.</p>
+    <p>Data Sources: <a href='https://kenpom.com/'>Kenpom</a> | <a href='https://barttorvik.com/#'>Torvik</a></p>
+    <p>Today's scores and schedule from: <a href='https://www.cbssports.com/college-basketball/scoreboard/'>CBS Sports</a></p>
+'''
+    html += "<h3>Men's Power 5 Games Today</h3>" + men_games + "<h3>Women's P5 Games Today</h3>" + women_games
     path =  utils.get_path('docs/index.html')
     html = htmb.add_front_matter(html, "GordStats Home")
 
