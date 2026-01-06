6edbf64f46d4fa0bb6d82e9dc9f70924d7330dcc
tylergordon3 <tmgordon33@gmail.com>
2026-01-05 22:26:38 -0500

    fixing render

---
+++ py/render/render_home.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup
+import utils
+import html_builder as htmb
+
+def render_home(men_rankings, men_games_html, women_rankings, women_games_html):
+    """
+    Creates index.html for home page of our website
+    
+    :param men_rankings: Men's DataFrame of current model rankings 
+    :type men_rankings: DataFrame
+    :param men_games: Today's slate of games and scores for men
+    :type men_games: String of HTML
+    :param women_rankings: Women's DataFrame of current model rankings 
+    :type women_rankings: DataFrame
+    :param women_games: Today's slate of games and scores for women
+    :type women_games: String of HTML
+    """
+
+    men_soup = BeautifulSoup(men_games_html, "html.parser")
+    women_soup = BeautifulSoup(women_games_html, "html.parser")
+
+    def getPower5(soup):
+        if (soup is None) | (soup.find('h3') is None):
+            return ""
+        h3 = soup.find('h3')
+        content = []
+        for elem in h3.next_siblings:
+            if elem.name == "h3":
+                break
+            content.append(str(elem))
+
+        if len(content) == 1:
+            return ""
+        result = "".join(content)
+        return result
+    
+    men_games = getPower5(men_soup)
+    women_games = getPower5(women_soup)
+    
+    if men_games == "":
+        men_games = "No men's power 5 games today."
+    if women_games == "":
+        women_games = "No women's power 5 games today."
+    html = '''
+    <p>Using machine learning to predict the NCAA March Madness field.</p>
+    <p>Data Sources: <a href='https://kenpom.com/'>Kenpom</a> | <a href='https://barttorvik.com/#'>Torvik</a></p>
+    <p>Today's scores and schedule from: <a href='https://www.cbssports.com/college-basketball/scoreboard/'>CBS Sports</a></p>
+'''
+    html += "<h3>Men's Power 5 Games Today</h3>" + men_games + "<h3>Women's P5 Games Today</h3>" + women_games
+    path =  utils.get_path('docs/index.html')
+    html = htmb.add_front_matter(html, "GordStats Home")
+
+    with open(path, "w") as f:
+        f.write(html)
+        print(f"Wrote to: {path}")
