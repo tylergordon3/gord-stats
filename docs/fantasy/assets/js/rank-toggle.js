408124f28966622bc031e71e0d43f43518b70870
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2026-01-07 17:17:52 +0000

    edit java

--- docs/fantasy/assets/js/rank-toggle.js
+++ docs/fantasy/assets/js/rank-toggle.js
@@ -5,24 +5,16 @@ function normText(s) {
     .trim();
 }
 
-function setChangeDiv(type, inj, btn) {
-  const options = {
-    "All": "all",
-    "Best": "best",
-    "Worst": "worst"
-  };
-
-  const inj_options = {
-    "Injuries" : "inj",
-    "No Injuries" : "noing"
-  };
+function setChangeT(type, btn) {
 
   document.querySelectorAll(".table-scroll").forEach(div => {
     div.classList.add("hidden-div");
   })
 
+  const inj = document.querySelectorAll(".active.injury-toggle").getAttribute('data-key')
+
   // Get all tables with the class "table-scroll"
-  const target = document.querySelector(`.table-scroll .${options[type]} .${inj_options[inj]}`);
+  const target = document.querySelector(`.table-scroll.${type}.${inj}`);
   if (target) target.classList.remove("hidden-div");
 
   // active button styling
@@ -30,39 +22,31 @@ function setChangeDiv(type, inj, btn) {
     document.querySelectorAll(".type-toggle button").forEach(b => b.classList.remove("active"));
     if (btn) btn.classList.add("active");
   }
-  
+}
+
+function setChangeI(inj, btn) {
+
+  document.querySelectorAll(".table-scroll").forEach(div => {
+    div.classList.add("hidden-div");
+  })
+
+  const type = document.querySelectorAll(".active.type-toggle").getAttribute('data-key')
+
+  // Get all tables with the class "table-scroll"
+  const target = document.querySelector(`.table-scroll.${type}.${inj}`);
+  if (target) target.classList.remove("hidden-div");
+
   // active button styling
-  if (btn.classList.contains("injury-toggle")) {
-    document.querySelectorAll(".injury-toggle button").forEach(b => b.classList.remove("active"));
+  if (btn.classList.contains("inj-toggle")) {
+    document.querySelectorAll(".inj-toggle button").forEach(b => b.classList.remove("active"));
     if (btn) btn.classList.add("active");
   }
 }
 
 // default = whatever button has class active (yours is 2 Weeks)
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".type-toggle button").forEach(btn => {
-        btn.addEventListener("click", () => {
-            const type = btn.getAttribute("data-key");
-            document.querySelectorAll(".injury-toggle button").forEach(btn => {
-                if (btn.classList.contains("active")){
-                    setChangeDiv(type, btn.getAttribute("data-key"), btn)
-                }
-            })
-        })
-    })
   const defaultTypeBtn = document.querySelector(".type-toggle button.active");
   if (defaultTypeBtn) defaultTypeBtn.click();
-
-  document.querySelectorAll(".injury-toggle button").forEach(btn => {
-        btn.addEventListener("click", () => {
-            const inj = btn.getAttribute("data-key");
-            document.querySelectorAll(".type-toggle button").forEach(btn => {
-                if (btn.classList.contains("active")){
-                    setChangeDiv(btn.getAttribute("data-key"), inj, btn)
-                }
-            })
-        })
-    })
   const defaultInjBtn = document.querySelector(".injury-toggle button.active");
   if (defaultInjBtn) defaultInjBtn.click();
 });
--- py/draft.py
+++ py/draft.py
@@ -194,9 +194,9 @@ time = time_obj.strftime("Last Update: %A %m/%d/%y %I:%M %p")
 df_html = f"<p>{time}</p>"
 df_html +=  f'''
             <div class="type-toggle">
-                <button data-key="all" class="active type-toggle">All</button>
-                <button data-key="best" class="type-toggle>Best</button>
-                <button data-key="worst" class="type-toggle>Worst</button>
+                <button data-key="all" onclick="setChangeT('all', this)" class="active type-toggle">All</button>
+                <button data-key="best" onclick="setChangeT('best', this)" class="type-toggle>Best</button>
+                <button data-key="worst" onclick="setChangeT('worst', this)" class="type-toggle>Worst</button>
             </div>
             <div class="injury-toggle">
             <button data-key="injuries" class="active injury-toggle">Injuries</button>
