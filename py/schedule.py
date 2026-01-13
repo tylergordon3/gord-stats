61ecd77ddb0bbf08f1f69438e672ee7e45678e37
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2026-01-13 21:40:07 +0000

    edit agan

--- py/schedule.py
+++ py/schedule.py
@@ -26,6 +26,9 @@ def schedule_main():
 
     # All-Play Stats
     html += '<h2>Records vs Every Schedule</h2>'
+    html += '<p>How To Read:</p>'
+    html += '<p>Left to right - All teams (columns) compared to 1 schedule (row) </p>'
+    html += '<p>Top to bottom - 1 team (column) compared to every schedule (row)</p> '
     html += '<div class="table-scroll">'
     html += schedule_stats.schedule_compare().to_html(index=False)
     html += '</div>'
