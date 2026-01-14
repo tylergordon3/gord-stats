12695e6df585b0ce28cf54e5776acb9119ecae5c
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2026-01-14 20:52:55 +0000

    adding to new master with short naming

--- py/sandbox.py
+++ py/sandbox.py
@@ -4,52 +4,15 @@ import os
 import pandas as pd
 import scraper
 
-def upd():
-    master = scraper.getMasterTeams()
-    for index, row in master.iterrows():
-        new = row['team'].lower()
-        new = new.replace('.', '')
-        new = new.replace('\'', '')
-        namearr = row['names']
-        if new in row.names:
-            pass
-        else:
-            namearr.append(new)
-            master.at[index, 'names'] = namearr
-    scraper.saveMasterTeams(master)
-
-path = utils.get_path('docs/assets/images')
-
-directory = os.listdir(path)
-master = scraper.getMasterTeams()
-
-master['image'] = sorted(directory)
-
-master['image'] = master['image'].apply(lambda x: x[:-4])
-
-def check(row):
-    s_exploded = master["names"].explode()
-    boolean_mask_exploded = s_exploded == row.image
-    # To get the row IDs where the value is present:
-    matching_ids = s_exploded[boolean_mask_exploded].index.unique()
-    boolean_mask_original = boolean_mask_exploded.groupby(level=0).any()
-    df_result = master[boolean_mask_original]
-   
-    if df_result.empty:
-        return ''
-    else:
-        return list(matching_ids)[0]
-    
-master['team'] = master['team'].replace('.','')
-master['check'] = master.apply(lambda x: check(x), axis=1)
-#print(master[master['check'] == ''])
-master = master.drop(columns=['names'])
-
-test = master[['image', 'check']].copy()
-
-test['check'] = pd.to_numeric(test['check'])
-test = test.sort_values(by='check')
-print(test.to_string())
+master = pd.read_json(utils.get_path("data/teams/master_new.json"))
 
 
+#master['short'] = ''
+# df.loc[row, col]
+team = "Southern Indiana"
+short = 'USI'
+master.loc[master['team'] == team, 'short'] = short
+print(master[['team', 'short']].to_string())
+
+master.to_json(utils.get_path("data/teams/master_new.json"))
 
