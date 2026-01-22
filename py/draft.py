8808281367e4f4a01be4cc7cfdb1bb40a07405a5
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2026-01-22 18:35:55 +0000

    trying to normalize background gradient

--- py/draft.py
+++ py/draft.py
@@ -70,11 +70,12 @@ def get_over(pick_str):
     pick_no = match.group(2)
     return pick_no
 
-def default_style(df, cols, opt_styler="RdYlGn"):
+def default_style(df, cols, opt_styler="RdYlGn", quant=1):
+    limit = max(df[cols].quantile(quant))
     return (df
         .style
         .hide(axis="index") 
-        .background_gradient(cmap=opt_styler, subset=cols))
+        .background_gradient(cmap=opt_styler, subset=cols, vmax=limit))
 
 def table_html(styler):
     return f'''<div class="table-scroll">
@@ -160,7 +161,7 @@ df_lottery_no_injuries = df_lottery[~((df_lottery['Games Played'] < 10))]
 df_best = df.sort_values(by='Overall Rank Δ', ascending=False)
 df_worst = df.sort_values(by='Overall Rank Δ')
 
-styler = default_style(df, ["Pos. Rank Δ", "Overall Rank Δ"])
+styler = default_style(df, ["Pos. Rank Δ", "Overall Rank Δ"], quant=.85)
 styler_best = default_style(df_best.head(CUTOFF_ROWS), ["Overall Rank Δ"], opt_styler='Greens')
 styler_worst = default_style(df_worst.head(CUTOFF_ROWS), ["Overall Rank Δ"], opt_styler='Reds_r')
 
