2620051004dc56f6b16de715aed5d71ed4b6bec0
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2026-01-27 19:59:46 +0000

    saving today kenpom data

--- py/kenpom.py
+++ py/kenpom.py
@@ -5,13 +5,16 @@ Documentation:
 '''
 import kenpom_wrapper
 import pandas as pd
+import utils
 from dotenv import load_dotenv
-from functools import reduce
+from datetime import datetime
+import pytz
 
 load_dotenv() 
 kenpom = kenpom_wrapper.KenpomData()
 
 def kenpom_now():
+    now = datetime.now().replace(tzinfo=pytz.timezone("US/Eastern")).strftime('%Y-%m-%d')
     ratings = pd.DataFrame(kenpom.get_ratings(2026))
     ff = pd.DataFrame(kenpom.get_four_factors(2026))
     dist = pd.DataFrame(kenpom.get_point_distribution(2026))
@@ -22,5 +25,6 @@ def kenpom_now():
     merge2 = pd.merge(merge1, dist, how='outer')
     merge3 = pd.merge(merge2, height, how='outer')
     combo = pd.merge(merge3, misc, how='outer')
-    print(combo.columns)
+    path = utils.get_path(f"data/men/kenpom_api/{now}.json")
+    utils.save_json_data(combo.to_json(), path)
 kenpom_now()
