193719c2d0ae8c40546e56dc79815a5b1a23af0f
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2026-01-27 19:47:23 +0000

    kenpom api further set

--- py/kenpom.py
+++ py/kenpom.py
@@ -4,9 +4,23 @@ Documentation:
     https://kenpom.com/api-documentation.php
 '''
 import kenpom_wrapper
+import pandas as pd
 from dotenv import load_dotenv
+from functools import reduce
 
 load_dotenv() 
-
 kenpom = kenpom_wrapper.KenpomData()
-print(kenpom.get_ratings(2025))
+
+def kenpom_now():
+    ratings = pd.DataFrame(kenpom.get_ratings(2026))
+    ff = pd.DataFrame(kenpom.get_four_factors(2026))
+    dist = pd.DataFrame(kenpom.get_point_distribution(2026))
+    height = pd.DataFrame(kenpom.get_point_distribution(2026))
+    misc = pd.DataFrame(kenpom.get_misc_stats(2026))
+
+    merge1 = pd.merge(ratings, ff, how='outer')
+    merge2 = pd.merge(merge1, dist, how='outer')
+    merge3 = pd.merge(merge2, height, how='outer')
+    combo = pd.merge(merge3, misc, how='outer')
+    print(combo.columns)
+kenpom_now()
