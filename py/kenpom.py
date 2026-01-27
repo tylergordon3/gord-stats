97910a7111c0060f7513acf4aa7f08c443de005a
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2026-01-27 14:43:12 +0000

    add constants and docstring to kenpom api parse

--- py/kenpom.py
+++ py/kenpom.py
@@ -1,3 +1,8 @@
+'''
+Docstring for py.kenpom
+Documentation:
+    https://kenpom.com/api-documentation.php
+'''
 import os
 from dotenv import load_dotenv
 
@@ -5,3 +10,7 @@ load_dotenv()
 
 KENPOM_KEY = os.getenv("KENPOM")
 BASE_URL = "https://kenpom.com/api.php"
+HEADERS = {
+        "Authorization": f"Bearer {KENPOM_KEY}",
+        "Accept": "application/json"
+    }
