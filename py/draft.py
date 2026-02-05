7856a07ed4b09314e0a9800b5af7654d33827614
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2026-02-05 19:19:46 +0000

    amending link

--- py/draft.py
+++ py/draft.py
@@ -306,7 +306,6 @@ def main(season_str):
     time = time_obj.strftime("Last Update: %A %m/%d/%y %I:%M %p")
     df_html = f"<p>{time}</p>"
     df_html += '<p>Δ = Delta = Change/Difference</p>'
-    df_html += "<a href='draft_team.html'>Team Draft Breakdown</a>"
     df_html +=  f'''
         <p>Note: Does not include defenses or kickers.</p>
         <details>
