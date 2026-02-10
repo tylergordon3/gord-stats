40a7b0bcceb6b45b3d5cbc2f9a17256c7d463769
tylergordon3 <tmgordon33@gmail.com>
2026-02-09 21:14:01 -0500

    roth twit added

--- py/render/render_home.py
+++ py/render/render_home.py
@@ -3,14 +3,52 @@ import utils
 import html_builder as htmb
 
 def render_home():
-    html = '''
+    # Use a raw string (r''') to handle any special characters safely
+    html = r'''
     {% include countdown.html %} 
-    <p>Using machine learning to predict the NCAA March Madness field.</p>
-    <p>Data Sources: <a href='https://kenpom.com/'>Kenpom</a> | <a href='https://barttorvik.com/#'>Torvik</a></p>
-    <p>Today's scores and schedule from: <a href='https://www.cbssports.com/college-basketball/scoreboard/'>CBS Sports</a></p>
-'''
-    html += "See the scores tab for men's scoreboard."
-    path =  utils.get_path('docs/index.html')
+    <div class="home-grid">
+      <div class="home-left">
+        <h1>GordStats Home</h1>
+        <p>Using machine learning to predict the NCAA March Madness field.</p>
+        <p>
+          Data Sources:
+          <a href="https://kenpom.com/">Kenpom</a> |
+          <a href="https://barttorvik.com/#">Torvik</a>
+        </p>
+        <p>
+          Today's scores and schedule from:
+          <a href="https://www.cbssports.com/college-basketball/scoreboard/">CBS Sports</a>
+        </p>
+        <p>See the scores tab for men's scoreboard.</p>
+      </div>
+
+      <div class="home-right">
+        <div class="twitter-title">Live Updates</div>
+        <a class="twitter-timeline"
+           data-theme="dark"
+           data-height="520"
+           href="https://twitter.com/JonRothstein?ref_src=twsrc%5Etfw">
+           Tweets by JonRothstein
+        </a>
+      </div>
+    </div>
+    
+   <script>
+  // This tells X to scan the page for the "twitter-timeline" link again
+  window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0], t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    t._e = []; t.ready = function(f) { t._e.push(f); };
+    return t;
+  }(document, "script", "twitter-wjs"));
+</script>
+    '''
+    # Note: Removed the redundant "html += ..." line as it's already in the block above.
+    
+    path = utils.get_path('docs/index.html')
     html = htmb.add_front_matter(html, "GordStats Home")
 
     with open(path, "w") as f:
