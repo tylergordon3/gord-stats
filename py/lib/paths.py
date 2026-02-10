e006a889626dd340d9ffe8c069b59497fef30e0d
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2026-02-10 22:36:15 +0000

    edit

---
+++ py/lib/paths.py
@@ -0,0 +1,8 @@
+'''
+Holds Pathing information as constants
+'''
+from pathlib import Path
+
+DATA_BASE = ''
+
+print(DATA_BASE)
---
+++ py/lib/url.py
@@ -0,0 +1,12 @@
+'''
+Store links as constants
+'''
+
+NCAAM_BPI = "https://site.web.api.espn.com/apis/fitt/v3/sports/basketball/mens-college-basketball/powerindex"
+NCAAM_ATS = "https://www.teamrankings.com/ncb/trends/ats_trends/"
+NCAAM_NET = "https://www.ncaa.com/rankings/basketball-men/d1/ncaa-mens-basketball-net-rankings"
+NCAAM_KEN = "https://kenpom.com/api.php"
+
+NCAAW_NET = "https://www.ncaa.com/rankings/basketball-women/d1/ncaa-womens-basketball-net-rankings"
+
+LIVE_SCORES = "https://api.thescore.com"
