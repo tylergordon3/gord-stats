575fed40c5c8bbd386eea954a49c5babddfeb654
tylergordon3 <tmgordon33@gmail.com>
2026-02-09 19:49:54 -0500

    edit to countdown

--- py/render/render_home.py
+++ py/render/render_home.py
@@ -4,12 +4,12 @@ import html_builder as htmb
 
 def render_home():
     html = '''
+    {% include countdown.html %} 
     <p>Using machine learning to predict the NCAA March Madness field.</p>
     <p>Data Sources: <a href='https://kenpom.com/'>Kenpom</a> | <a href='https://barttorvik.com/#'>Torvik</a></p>
     <p>Today's scores and schedule from: <a href='https://www.cbssports.com/college-basketball/scoreboard/'>CBS Sports</a></p>
 '''
     html += "See the scores tab for men's scoreboard."
-    html +=  '''{% include countdown.html %} '''
     path =  utils.get_path('docs/index.html')
     html = htmb.add_front_matter(html, "GordStats Home")
 
