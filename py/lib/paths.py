5aadd967aa51695f19b366b087b9551d6fd2e421
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2026-02-11 19:24:49 +0000

    adding to paths file, copying ref dict to new folder for better clarify

--- py/lib/paths.py
+++ py/lib/paths.py
@@ -3,6 +3,38 @@ Holds Pathing information as constants
 '''
 from pathlib import Path
 
-DATA_BASE = ''
+# -----
+# Bases
+# -----
+ROOT = Path("cbb-model")
+SRC = ROOT / Path("py")
+DATA = ROOT / Path("data")
+DOCS = ROOT / Path("docs")
 
-print(DATA_BASE)
+# ---------------
+# Data Collection
+# ---------------
+MEN_DATA = DATA / Path("men")
+WOMEN_DATA = DATA / Path("women")
+OTHER_DATA = DATA / Path("refs")
+
+MEN_KENPOM = MEN_DATA / Path("kenpom_api")
+MEN_KENPOM_OLD = MEN_DATA / Path("kenpom")
+MEN_ATS = MEN_DATA / Path("ats")
+MEN_ESPN = MEN_DATA / Path("espn")
+MEN_NET = MEN_DATA / Path("net")
+MEN_TORVIK = MEN_DATA / Path("torvik")
+
+WOMEN_TORVIK = WOMEN_DATA / Path("torvik")
+WOMEN_NET = WOMEN_DATA / Path("net")
+
+# --------------
+# Website Assets
+# --------------
+ASSETS = DOCS / Path("assets")
+CSS = ASSETS / Path("css")
+ASSET_DATA = ASSETS / Path("data")
+
+MASTER_DICT = ASSET_DATA / Path("master.json")
+MEN_RANK_DICT = ASSET_DATA / Path("ranks.json")
+WOMEN_RANK_DICT = ASSET_DATA / Path("wranks.json")
