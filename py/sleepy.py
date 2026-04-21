3802008f928edc81d069b07e861d3a48ac1e48b3
tylergordon3 <91852588+tylergordon3@users.noreply.github.com>
2026-04-21 20:13:23 +0000

    edt

--- py/sleepy.py
+++ py/sleepy.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import constants as c
-
+import json
 from sleeper_wrapper import League
 
 league = League(c.LEAGUEID_2025)
@@ -15,4 +15,15 @@ roster_df = roster_df.rename(columns={'owner_id' : 'user_id'})
 
 info = pd.merge(user_df, roster_df, 'inner', 'user_id')
 info = info[['user_id', 'display_name', 'team_name', 'roster_id']].copy()
+
+with open('data/players.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+    df = pd.DataFrame.from_dict(data, orient='index')
+
+
+
+players = df[['full_name', 'search_first_name', 'injury_start_date', 'number', 'active', 'years_exp', 'last_name', 
+              'search_last_name', 'weight', 'status', 'height', 'injury_notes', 'team_changed_at', 'injury_status', 
+              'search_full_name', 'college', 'depth_chart_order', 'practice_participation', 
+              'player_id', 'team', 'first_name', 'position', 'search_rank', 'age']].copy()
 
